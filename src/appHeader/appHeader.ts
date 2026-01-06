24abafecd525c9488546a3f1521b96367bee7639
deanitz <deanitz@gmail.com>
2026-01-06 11:47:16 +0300

    add empty app header component

---
+++ src/appHeader/appHeader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './appHeader.html',
+  styleUrl: './appHeader.scss',
+})
+export class AppHeader {
+
+}
